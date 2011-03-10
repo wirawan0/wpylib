f64a3cfd8e47cf1bfecaf9eac5956c0abc06f735
wirawan <wirawan>
2011-03-10 20:18:27 +0000

    * Using external login node on jaguar.

--- update-jaguar.sh
+++ update-jaguar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -av . jaguar:/ccs/home/wirawan0/wpylib/. -f '+ *.py' -f '- CVS/' -f '+ /**/' -f '- *'
+rsync -av . jaguar-ext:/ccs/home/wirawan0/wpylib/. -f '+ *.py' -f '- CVS/' -f '+ /**/' -f '- *'
