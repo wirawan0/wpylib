26db0806aebe6a26262a6b66a8486c099cf68383
wirawan <wirawan>
2011-03-10 19:22:37 +0000

    * Make Struct available publicly in wpylib.params namespace (with the   'struct' name).

--- params/__init__.py
+++ params/__init__.py
@@ -1,10 +1,19 @@
-# $Id: __init__.py,v 1.1 2010-09-30 16:16:38 wirawan Exp $
+# $Id: __init__.py,v 1.2 2011-03-10 19:22:37 wirawan Exp $
 #
 # wpylib.params module
 # Created: 20100930
 # Wirawan Purwanto
 
-"""Parameter-related stuff."""
+"""Parameter-related stuff.
+
+Exported names:
+
+* flat - flat parameter namespace (but with a sequence of dicts/namespaces
+  to look value from).
+* struct - struct-like container. Do not confuse with python's built-in
+  'struct' class!
+"""
 
 from wpylib.params.params_flat import Parameters as flat
+from wpylib.params.params_struct import Struct as struct
 
