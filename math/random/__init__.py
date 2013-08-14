42fdcc5253208a51e0cea9141530f8e46b52c905
Wirawan Purwanto <wirawan0@gmail.com>
2013-08-14 15:29:26 -0400

    * Added new module wpylib.math.random and its submodule (rng_lcg48).   The LCG48 generator is primarily for testing algorithms involving random   numbers.

---
+++ math/random/__init__.py
@@ -0,0 +1,13 @@
+#
+# wpylib.math.random module
+# Created: 20130814
+# Wirawan Purwanto
+#
+
+import numpy
+
+class rng_base(object):
+  """Bas class for random number generator."""
+
+# Standard classes
+from wpylib.math.random.rng_lcg48 import lcg48
