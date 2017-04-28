7c34fca12c6087ea0da9428066bc9992b52f028b
Wirawan Purwanto <wirawan0@gmail.com>
2017-04-28 18:29:56 -0400

    * Added more methods for reverse-lookup of an object's name.

--- py/introspection.py
+++ py/introspection.py
@@ -7,25 +7,43 @@
 # Advanced introspection tools for python
 #
 
-def name_rlookup(val, namespace, prefix=None, rx_match=None, lookup="dict"):
+def name_rlookup(val, namespace, prefix=None, rx_match=None, lookup="dict", comp="=="):
   """Reverse lookup of variable/object name.
-
-  Yields the list of object names in the namespace whose respective values are
-  equal to `val`.
+  This function yields the list of object names in the namespace whose
+  respective values are equal to `val`.
 
   This is useful for example to reverse-lookup an object's name, but only in
   restricted contexts (e.g. when the value to look for is unique).
 
-  `prefix` or `rx_match` can be given to narrow the scope of names to look for.
-  The two cannot be given at once; `prefix` will take precedence in that case.
+  Input:
+
+  * `val`:: Value to compare against (or to search the key/name for)
+
+  * `namespace`:: The namespace object, which can be a dict-like object or an
+    object whose attributes can be enumerated using dir() function.
+
+  * `prefix` or `rx_match`:: Only one can be given to narrow the scope of the names
+    to search from.
+    The two cannot be given at once; `prefix` will take precedence in that case.
+
+  *  `lookup`:: The namespace lookup method can be 'dict' (for dict-like value lookup)
+     or 'attr' (for attribute-like lookup).
+
+  * `comp`:: The value comparison method: either a '==' or 'is' string, to use that
+    python operator in the comparison; or use a custom 2-argument function that takes
+    two input values to compare and returns True (False) if the two values match
+    (don't match).
   """
   # Imported 20150819
   # Original subroutine name: search_var_name (from Cr2_analysis_cbs.py).
   if lookup == "dict":
     names = namespace.keys()
-  else:
+  elif lookup in ("attr", "attribute", "class"):
     # attribute lookup
     names = dir(namespace)
+  else:
+    raise ValueError, "Invalid `lookup` argument"
+
   if prefix is not None:
     names_filt = [ n for n in names if n.startswith(prefix) ]
   elif rx_match is not None:
@@ -39,14 +57,24 @@ def name_rlookup(val, namespace, prefix=None, rx_match=None, lookup="dict"):
   else:
     names_filt = names
   names_match = []
-  type_val = type(val)
+
+  if callable(comp):
+    # TODO: make sure it is a 2-arg function
+    pass
+  elif comp == "==":
+    comp = lambda v, val: (type(v) is type(val)) and (v == val)
+  elif comp == "is":
+    comp = lambda v, val: (type(v) is type(val)) and (v is val)
+  else:
+    raise ValueError, "Invalid `comp` argument"
+
   if lookup == "dict":
     for n in names_filt:
       try:
         v = namespace[n]
       except:
         continue
-      if type(v) == type_val and v == val:
+      if comp(v, val):
         names_match.append(n)
   else:
     for n in names_filt:
@@ -54,7 +82,7 @@ def name_rlookup(val, namespace, prefix=None, rx_match=None, lookup="dict"):
         v = getattr(namespace, n)
       except:
         continue
-      if type(v) == type_val and v == val:
+      if comp(v, val):
         names_match.append(n)
   return names_match
 
